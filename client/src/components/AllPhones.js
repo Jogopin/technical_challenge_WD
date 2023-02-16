fa15f3ae9c12fca3cb1329a62f0afd8c9c798da9
jogopin <jonn.g.pineda@gmail.com>
2023-02-16 18:48:50 +0100

    add basic css$ basic responsiveness

--- client/src/components/AllPhones.js
+++ client/src/components/AllPhones.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import "./AllPhones.css"
 export default function AllPhones(props){
     
     const {allPhonesData} = props
--- client/src/components/PhoneDetails.js
+++ client/src/components/PhoneDetails.js
@@ -1,48 +1,62 @@
 import { Link } from "react-router-dom";
-import { useParams} from "react-router-dom";
-import axios from "axios"
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { useEffect, useState } from "react";
-export default function PhoneDetails(props){
+import "./PhoneDetails.css";
+
+export default function PhoneDetails(props) {
     
-    const {API_URL} = props
-    const {phoneId} = useParams()
-    const [phoneObj,SetPhoneObj] = useState(null)
-    
-    const getPhoneDetails = ()=>{
-        axios.get(`${API_URL}/phones/${phoneId}`)
-            .then(responseAxios=>{
-                
-                SetPhoneObj(responseAxios.data[0])
-            })
-            .catch(e=>{
-                console.log("error getting the details of a phone",e)
-            })
-    }
+  const { API_URL } = props;
+  const { phoneId } = useParams();
+  const [phoneObj, SetPhoneObj] = useState(null);
 
-    useEffect(()=>{
-        getPhoneDetails()
-    },[phoneId])
+  const getPhoneDetails = () => {
+    axios
+      .get(`${API_URL}/phones/${phoneId}`)
+      .then((responseAxios) => {
+        SetPhoneObj(responseAxios.data[0]);
+      })
+      .catch((e) => {
+        console.log("error getting the details of a phone", e);
+      });
+  };
 
-    const renderPhoneDetails = ()=>{
+  useEffect(() => {
+    getPhoneDetails();
+  }, [phoneId]);
 
-        return(<div className="phone-details">
-                <div className="img-container"><img src={require(`../../public/images/${phoneObj.imageFileName}`)}  alt={phoneObj.imageFileName} /></div>
-                <h3>{phoneObj.name}</h3>
-                <h4>manufacter: {phoneObj.manufacturer}</h4>
-                <p>{phoneObj.description}</p>
-                <p>color: {phoneObj.color}</p>
-                <p>price: {phoneObj.price}</p>
-                <p>screen: {phoneObj.screen}</p>
-                <p>processor: {phoneObj.procesor}</p>
-                <p>ram: {phoneObj.ram}</p>
-    
+  const renderPhoneDetails = () => {
+    return (
+      <div className="phone-details">
+        <div className="img-container">
+          <img
+            src={require(`../../public/images/${phoneObj.imageFileName}`)}
+            alt={phoneObj.imageFileName}
+          />
+        </div>
+        <div className="phone-details-text">
+          <h2>{phoneObj.name}</h2>
+          <h4>manufacter: {phoneObj.manufacturer}</h4>
+          <p>{phoneObj.description}</p>
+          <p>
+            <b>color:</b> {phoneObj.color}
+          </p>
+          <p>
+            <b>price:</b> {phoneObj.price}
+          </p>
+          <p>
+            <b>screen:</b> {phoneObj.screen}
+          </p>
+          <p>
+            <b>processor:</b> {phoneObj.procesor}
+          </p>
+          <p>
+            <b>ram:</b> {phoneObj.ram}
+          </p>
+        </div>
+      </div>
+    );
+  };
 
-            </div>)
-    }
-  
-    return(<>
-
-        {phoneObj ? renderPhoneDetails() : "loading...."}
-
-    </>)
+  return <>{phoneObj ? renderPhoneDetails() : "loading...."}</>;
 }
